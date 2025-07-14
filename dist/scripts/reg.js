fd558b097cc9367b239ba1a7791a8672cd29927c
TempestaTorres <142347458+TempestaTorres@users.noreply.github.com>
2025-07-14 21:23:39 +0300

    register page finished

---
+++ dist/scripts/reg.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    'use strict';
+
+    function formValidate(e) {
+
+        alert('Valid');
+    }
+
+    // Entry point
+    validateForm(formValidate);
+});
